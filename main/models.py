a6422c007fa45ee7720e745177b67506a257807e
Ubuntu <ubuntu@ip-172-31-14-119.us-west-2.compute.internal>
2014-06-02 22:59:42 +0000

    config static files

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=63)
     email = models.EmailField(max_length=255)
-    intention = models.CharField(max_length=255)
+    intention = models.CharField(max_length=511)
     phone = models.CharField(default='', max_length=15)
 
     class Meta:
